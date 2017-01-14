fa1c6b354f7db0179c0dca5aac10488c762abef7
FlowerCake13 <lminami23@punahou.edu>
2017-01-14 10:53:17 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var player;
 var obstacle;
 
 //This sets the score to start at -1.
-var score = -1;
+var score = 0;
 
 
 var GAME_WIDTH = 800;
@@ -23,6 +23,7 @@ function preload(){
   game.load.image('player', 'assets/player.png');
   game.load.image('ground', 'assets/wallHorizontal.png');
   game.load.image('obstacle', 'assets/wallVertical.png');
+  game.load.image('coin', 'assets/coin.png');
 
   //If you'd like to load music files, the format would look like  game.load.audio('[name of music]', ['[location for music file]']);
 }
@@ -65,12 +66,16 @@ function create() {
 
   //This adds the scoreboard on the top left side of the screen.
   	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    coin = game.add.sprite(500, 500, 'coin');
+    game.physics.arcade.enable(coin);
 };
 
 function update(){
   //This is where the game engine recognizes collision betwen the player and the ground or the obstacle.
 	game.physics.arcade.collide(player, ground);
 	game.physics.arcade.collide(player, obstacle);
+  game.physics.arcade.overlap(player, coin, colllectCoin, null, this);
 
   //This allows the player to jump only if you press the space key and the player is touching the something at the bottom.
   	if (spaceKey.isDown && player.body.touching.down) {
@@ -83,6 +88,10 @@ function update(){
   		obstacle.x -= 0.05;
   	}
 
+    if (coin.x > 1) {
+      coin.x -= 0.05;
+    }
+
   //This will create a new wall if the old wall goes off the screen
   	if (obstacle.x < 0) {
   		obstacle.kill();
@@ -92,6 +101,25 @@ function update(){
   		game.physics.arcade.enable(obstacle);
   		obstacle.body.immovable = true;
   	}
+
+    function colllectCoin(){
+      console.log("eawiuefwuwaefiuwuiw")
+      coin.kill();
+      score++;
+      scoreText.text = 'score: ' + score;
+      makeCoin();
+    }
+
+    function makeCoin(){
+      console.log('coin')
+        coin.kill();
+        coin = game.add.sprite(800, 500, 'coin');
+        game.physics.arcade.enable(coin);  
+    }
+
+    if (coin.x < 0) {
+     makeCoin();
+    }
 
   //This will update the score if the player has not been pushed off the screen, and the wall has gone off the left side.
   	if (obstacle.x < 5 && player.x > 5) {
